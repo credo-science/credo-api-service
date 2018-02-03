21222b6a8a675252a872ca9bfd2c230eae976e5c
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-02-03 20:26:39 +0100

    Add basic amqp message publishing.

--- credo-api-service/app.py
+++ credo-api-service/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, g
 
 from api import api
 
 app = Flask(__name__)
 app.register_blueprint(api)
+
+
+@app.teardown_appcontext
+def close_rabbitmq_connection(exception):
+    if hasattr(g, 'rabbitmq_lock'):
+        g.rabbitmq_lock.acquire()
+        g.rabbitmq_connection.close()
+        delattr(g, 'rabbitmq_channel')
+        delattr(g, 'rabbitmq_connection')
+        delattr(g, 'rabbitmq_lock')
+        g.rabbitmq_lock.release()
+
--- credo-api-service/handler.py
+++ credo-api-service/handler.py
@@ -1,10 +1,40 @@
 from validator import validate, ValidationException
+from flask import g
+import pika
+import config
+from threading import Lock
+
+EXCHANGE_NAME = 'detections'
+QUEUE_NAME = 'detections_save'
 
 
 def handle_detection(message):
     validate(message)
-    sendmessage(message)
+    send_message(message)
 
 
-def sendmessage(message):
-    pass
+def send_message(message):
+    # TODO: move to app context initialization?
+    if not hasattr(g, 'rabbitmq_lock'):
+        g.rabbitmq_lock = Lock()
+        g.rabbitmq_lock.acquire()
+        g.rabbitmq_connection, g.rabbitmq_channel = connect_rabbitmq()
+        g.rabbitmq_lock.release()
+
+    g.rabbitmq_lock.acquire()
+    g.rabbitmq_channel.basic_publish(exchange=EXCHANGE_NAME, body=message)
+    g.rabbitmq_lock.release()
+
+
+def connect_rabbitmq():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.RMQ_HOST))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type='fanout')
+
+    result = channel.queue_declare(queue=QUEUE_NAME, durable=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange=EXCHANGE_NAME, queue=queue_name)
+
+    return connection, channel
